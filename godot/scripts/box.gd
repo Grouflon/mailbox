2cada94dc5cac6f327a172da57bacd70e5f640fc
Memuth <remi.bismuth@gmail.com>
2026-04-02 15:54:30 +0200

    interactive box

---
+++ godot/scripts/box.gd
@@ -0,0 +1,26 @@
+extends Node
+class_name Box
+
+@export var animation_player: AnimationPlayer
+
+var opened: bool = false
+
+func set_closed():
+	animation_player.play(&"closed")
+	opened = false;
+	pass
+	
+func set_opened():
+	animation_player.play(&"opened")
+	opened = true;
+	pass
+	
+func open():
+	animation_player.play(&"open", -1, 2.0)
+	opened = true;
+	pass
+	
+func close():
+	animation_player.play(&"open", -1, -2.0, true)
+	opened = false;
+	pass
--- godot/scripts/game.gd
+++ godot/scripts/game.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node3D
 
 @export var notification_button: BaseButton;
 @export var permissions_button: BaseButton;
@@ -8,20 +8,29 @@ extends Node
 
 @export var delay_label: Label;
 @export var delay_slider: HSlider;
+@export var box: Box;
+@export var camera: Camera3D;
 
 const ANDROID_PLUGIN_NAME: = "MailboxAndroidPlugin"
 
 var android_plugin: Object;
+
+var pending_click: bool
+var pending_click_position: Vector2
 
 func _ready() -> void:
 	if Engine.has_singleton(ANDROID_PLUGIN_NAME):
 		android_plugin = Engine.get_singleton(ANDROID_PLUGIN_NAME)
 		android_plugin.connect("post_notifications_permission_result_received", on_post_notifications_permission_result_received)
 	
+	if android_plugin == null:
+		notification_button.disabled = true;
+		permissions_button.disabled = true;
+		app_page_button.disabled = true;
+		hello_world_button.disabled = true;
+		delay_slider.editable = false;
+	
 	text.text = "uninitialized"
-	#notification_button.disabled = true;
-	#permissions_button.disabled = true;
-	#app_page_button.disabled = true;
 	notification_button.pressed.connect(on_notification_button_pressed)
 	permissions_button.pressed.connect(on_permissions_button_pressed)
 	app_page_button.pressed.connect(on_app_page_button_pressed)
@@ -31,7 +40,47 @@ func _ready() -> void:
 	delay_slider.value_changed.connect(on_delay_value_changed)
 	
 	text.text = "initialiazing"
+	box.set_closed()
 	
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch:
+		pass
+	elif event is InputEventMouse && (event.button_mask & MOUSE_BUTTON_MASK_LEFT):
+		pass
+	else:
+		return
+	if not event.is_pressed():
+		return
+	
+	pending_click_position = event.position
+	pending_click = true
+	
+func _physics_process(delta: float) -> void:
+	
+	if pending_click:
+		var ray_origin: = camera.project_ray_origin(pending_click_position)
+		var ray_normal: = camera.project_ray_normal(pending_click_position)
+		#print("o:", ray_origin, "n:", ray_normal)
+		
+		var query: = PhysicsRayQueryParameters3D.create(ray_origin, ray_origin + ray_normal * 100.0)
+		query.collide_with_areas = true
+		var result: = get_world_3d().direct_space_state.intersect_ray(query)
+		#print("r:", result)
+		#
+		#if result:
+			#print("Hit at point: ", result.position)
+			
+		var collider = result.get("collider") as Area3D
+		if collider != null: 
+			var box: = collider.get_parent() as Box
+			if box != null:
+				if box.opened:
+					box.close()
+				else:
+					box.open()
+	
+	pending_click = false
+	pass
 
 func on_notification_button_pressed() -> void:
 	android_plugin.test_notifications()
